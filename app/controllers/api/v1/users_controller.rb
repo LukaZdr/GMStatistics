88f1df4a25b882498f559fe8b2b82a3f9ffbdc48
LukaZdr <luka.z@hotmail.de>
2021-01-31 16:18:23 +0100

    bug fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
     user = User.where(steam_id: params['user']['steam_id']).first_or_initialize
     server_error unless user.update(name: params['user']['name'])
 
-    login_time = LoginTime.new(user: user, time: epoch_to_datetime(params['time']))
+    login_time = LoginTime.new(user: user, login: epoch_to_datetime(params['time']))
     server_error unless login_time.save
 
     success
@@ -14,7 +14,7 @@ class Api::V1::UsersController < ApplicationController
 
 
   def logout
-    user = User.where(steam_id: params['user']["steam_id"])
+    user = User.find_by_steam_id(params['user']["steam_id"])
     server_error if user.current_login.update(logout: epoch_to_datetime(params['time']),
                                               logout_reason: params['user']['reason'])
     success
